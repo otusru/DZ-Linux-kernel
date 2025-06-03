ca39fac72693649d5d19402fa6516a6c57dbc54b
otusru <otusru@mail.ru>
2025-06-03 18:25:59 +0300

    Create hello_world.c

---
+++ src/hello_world.c
@@ -0,0 +1,24 @@
+#define KBUILD_MODNAME "hello"
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init hello_init(void)
+{
+	pr_info("Hello, World from the kernel!\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Goodbye, World from the kernel!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("otusru");
+MODULE_DESCRIPTION("A simple Hello World module for the Linux kernel");
