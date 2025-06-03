97ace66a01f3adb98d4c41d6071974afeb503882
otusru <otusru@mail.ru>
2025-06-03 19:59:04 +0300

    Update hello_param.c

--- src/hello_param.c
+++ src/hello_param.c
@@ -1,33 +1,36 @@
+#define KBUILD_MODNAME "hello_param"
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/stat.h>
 
-#define MAX_ARRAY_LEN 10  // Разрешим до 10 элементов
+#define MAX_ARRAY_LEN 10
 
-static short int myshort = 1;
+static short myshort = 1;
 static int myint = 420;
-static long int mylong = 9999;
-static char *mystring = "OTUS";
-static int myintarray[MAX_ARRAY_LEN] = {420, 420};
-static int arr_argc = 2;  // Установим соответствие значению по умолчанию
+static long mylong = 9999;
 
-module_param(myshort, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+static char mystring_buf[64] = "OTUS";
+static char *mystring = mystring_buf;
+
+static int myintarray[MAX_ARRAY_LEN] = {420, 420};
+static int arr_argc = 2;
+
+module_param(myshort, short, 0644);
 MODULE_PARM_DESC(myshort, "A short integer");
 
-module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+module_param(myint, int, 0644);
 MODULE_PARM_DESC(myint, "An integer");
 
-module_param(mylong, long, S_IRUSR);
+module_param(mylong, long, 0444);
 MODULE_PARM_DESC(mylong, "A long integer");
 
-module_param(mystring, charp, S_IRUGO);
+module_param(mystring, charp, 0444);
 MODULE_PARM_DESC(mystring, "A character string");
 
-module_param_array(myintarray, int, &arr_argc, S_IRUSR | S_IWUSR);
+module_param_array(myintarray, int, &arr_argc, 0644);
 MODULE_PARM_DESC(myintarray, "An array of integers (max " __stringify(MAX_ARRAY_LEN) ")");
 
 static int __init hello_param_init(void)
@@ -42,7 +45,8 @@ static int __init hello_param_init(void)
 	pr_info("mystring = %s\n", mystring);
 
 	if (arr_argc > MAX_ARRAY_LEN) {
-		pr_warn("Only first %d elements of myintarray will be used (received %d).\n", MAX_ARRAY_LEN, arr_argc);
+		pr_warn("Too many elements for myintarray! Only first %d will be used (received %d).\n",
+		        MAX_ARRAY_LEN, arr_argc);
 		arr_argc = MAX_ARRAY_LEN;
 	}
 
@@ -64,4 +68,4 @@ module_exit(hello_param_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("otusru");
-MODULE_DESCRIPTION("A simple Hello Param module for the Linux kernel");
+MODULE_DESCRIPTION("An improved Hello Param module for the Linux kernel");
