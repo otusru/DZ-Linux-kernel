89746f7f2ca5bb715da6916f2fe42c4577694e89
otusru <otusru@mail.ru>
2025-06-03 18:54:01 +0300

    Update main.py

--- src/checker/main.py
+++ src/checker/main.py
@@ -1,7 +1,6 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
 import argparse
-import os
 from checker import Checker
 
 def main():
@@ -9,20 +8,10 @@ def main():
     parser.add_argument('module_name', type=str, help='Имя модуля для проверки')
     args = parser.parse_args()
 
-    module_path = f"/sys/module/{args.module_name}"
-    if not os.path.isdir(module_path):
-        print(f"Ошибка: модуль '{args.module_name}' не загружен или не существует.")
-        exit(1)
-
-    checker = Checker(args.module_name)
-
-    try:
+    with Checker(args.module_name) as checker:
         print("Проверка str_buf:", checker.check_str_buf())
         print("Проверка ch_val:", checker.check_ch_val())
         print("Проверка idx:", checker.check_idx())
-    finally:
-        checker.close()
 
 if __name__ == '__main__':
     main()
-
